a4c84a71aeb878dc2163e92ac3fcf488b61144d4
Guy Doenyas <guy@Guys-MacBook-Pro-2.local>
2023-01-15 14:45:46 -0800

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" strategy="beforeInteractive"/>
-      <script src="../form.js" strategy="beforeInteractive"/>
+      
       <Head />
       <body>
         <Main />
