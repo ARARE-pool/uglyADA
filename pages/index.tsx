fbc5442bf3b51a10f3d9da110387ffa010ebc708
Guy Doenyas <guy@Guys-MacBook-Pro-2.local>
2023-01-15 14:54:09 -0800

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -282,7 +282,7 @@ async function buildTx() {
             </span>  
             </div>
             <button type="button" class="bn632-hover bn19" style={{}} onClick={async () => buildTx() }>Build Tx</button>
-            <br /><br />
+            <br/><br/>
             
             
             
