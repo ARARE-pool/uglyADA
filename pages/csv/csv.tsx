af6d4df80b8b61ba6a229173f40bdde85ef44aaf
Guy Doenyas <guy@Guys-MacBook-Pro-2.local>
2023-01-14 17:26:41 -0800

    fix

--- pages/csv/csv.tsx
+++ pages/csv/csv.tsx
@@ -12,18 +12,21 @@ import Papa from 'papaparse';
 const UploadCSV = () => {
   const [data, setData] = useState([]);
 
-  const handleUpload = (e) => {
+  const handleUpload = (e:any) => {
     let file = e.target.files[0];
-
+    
     Papa.parse(file, {
       header: false,
       complete: (results) => {
-        setData(results.data);
+        const resultStore = typeof results.data === 'string' 
+        ? results.data : []
+        setData(typeof results.data === 'string' 
+        ? results.data : []);
         console.log(results)
         console.log("Result lenght: ", results.data.length)
         for(let i=0; i<results.data.length; i++){
-          console.log("Address : ", results.data[i][0]); //use i instead of 0
-          console.log("Lovelace : ", results.data[i][1]); //use i instead of 0
+          console.log("Address : ", resultStore[i][0]); //use i instead of 0
+          console.log("Lovelace : ", resultStore[i][1]); //use i instead of 0
       }
       }
     });
@@ -37,11 +40,11 @@ const UploadCSV = () => {
       <table>
         <tbody>
           {data.map((row, index) => (
-            <tr key={index}>
-              {Object.values(row).map((cell, index) => (
-                <td key={index}>{cell}</td>
-              ))}
-            </tr>
+            // <tr key={index}>
+            //   {Object.values(row).map((cell, index) => (
+            //     <td key={index}>{cell}</td>
+            //   ))}
+            // </tr>
           ))}
         </tbody>
       </table>
