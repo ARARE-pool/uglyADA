3ee457d7681e03d1724949aa0ac7d01f0e19a1af
Guy Doenyas <guy@Guys-MacBook-Pro-2.local>
2023-01-15 14:46:58 -0800

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,8 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" strategy="beforeInteractive"/>
-      
       <Head />
       <body>
         <Main />
