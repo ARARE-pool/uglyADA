01f8534721deef7a5641c04f175354e4fa7228e6
Guy Doenyas <guy@Guys-MacBook-Pro-2.local>
2023-01-14 16:22:56 -0800

    deploy

---
+++ pages/csv/csv.tsx
@@ -0,0 +1,55 @@
+// import React, { useState } from 'react';
+import { useState } from "react";
+import type { NextPage } from "next";
+import { useWallet } from '@meshsdk/react';
+import { CardanoWallet } from '@meshsdk/react';
+import { BrowserWallet } from '@meshsdk/core';
+import { Transaction } from '@meshsdk/core';
+import { useWalletList } from '@meshsdk/react';
+import { BlockfrostProvider } from '@meshsdk/core';
+import Papa from 'papaparse';
+
+const UploadCSV = () => {
+  const [data, setData] = useState([]);
+
+  const handleUpload = (e) => {
+    let file = e.target.files[0];
+
+    Papa.parse(file, {
+      header: false,
+      complete: (results) => {
+        setData(results.data);
+        console.log(results)
+        console.log("Result lenght: ", results.data.length)
+        for(let i=0; i<results.data.length; i++){
+          console.log("Address : ", results.data[i][0]); //use i instead of 0
+          console.log("Lovelace : ", results.data[i][1]); //use i instead of 0
+      }
+      }
+    });
+  }
+
+
+
+  return (
+    <div>
+      <input type="file" onChange={handleUpload} />
+      <table>
+        <tbody>
+          {data.map((row, index) => (
+            <tr key={index}>
+              {Object.values(row).map((cell, index) => (
+                <td key={index}>{cell}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <br/><br/><br/><br/>
+      <p>{data.map((col,
+                  idx) => <div key={idx}>{col[0]}{col[1]}</div>)}</p>
+    </div>
+  )
+}
+
+export default UploadCSV;
